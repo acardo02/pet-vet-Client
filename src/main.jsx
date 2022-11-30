5c06e6310f8551ace45424c6798cbb416754a127
acardo02 <00037820@uca.edu.sv>
2022-11-29 21:54:25 -0600

    Fixing imports

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
-import { ToastContainer } from 'react-toastify'
 import App from './App'
 import { UserContextProvider } from './Contexts/UserContext'
 import './index.css'
